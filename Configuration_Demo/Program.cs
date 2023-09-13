02ecfc1ad906324e077d6ce25445d66a8949bd23
MaximeTR <maxime.tremblay-rheault@bdc.ca>
2023-09-13 10:59:35 -0400

    options config

--- Configuration_Demo/Program.cs
+++ Configuration_Demo/Program.cs
@@ -9,6 +9,49 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+
+
+/****************************************************************/
+
+// ***************** Reading Configuration AppSettings
+
+var validIssuerConfig = builder.Configuration.GetValue<string>("Security:ValidIssuer");
+Console.WriteLine("---------------");
+Console.WriteLine(validIssuerConfig);
+Console.WriteLine("---------------");
+
+
+var myKeyValue = builder.Configuration["MyKey"];
+var title = builder.Configuration["Position:Title"];
+var name = builder.Configuration["Position:Name"];
+var defaultLogLevel = builder.Configuration["Logging:LogLevel:Default"];
+Console.WriteLine($"MyKey value: {myKeyValue} \n" +
+                  $"Title: {title} \n" +
+                  $"Name: {name} \n" +
+                  $"Default Log Level: {defaultLogLevel}");
+
+
+// ************ Reading Configuration AppSettings Options Pattern
+
+var positionOptions = new PositionOptions();
+builder.Configuration.GetSection(PositionOptions.Position).Bind(positionOptions);
+
+Console.WriteLine($"Title: {positionOptions.Title} \n" +
+                  $"Name: {positionOptions.Name}");
+
+positionOptions = builder.Configuration.GetSection(PositionOptions.Position)
+    .Get<PositionOptions>();
+
+Console.WriteLine($"Title: {positionOptions.Title} \n" +
+                  $"Name: {positionOptions.Name}");
+
+// Allow to dependency inject ctor(IOptions<PositionOptions> options) --> 
+builder.Services.Configure<PositionOptions>(
+    builder.Configuration.GetSection(PositionOptions.Position));
+
+
+/****************************************************************/
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Configuration_Demo/readme.md
+++ Configuration_Demo/readme.md
@@ -17,3 +17,42 @@ dotnet user-secrets init
 
 dotnet user-secrets set mailpassword password1
 ```
+
+
+
+## Getting configs through builder.Configuration
+
+<h5>.GetValue<string></h5>
+```
+var validIssuerConfig = builder.Configuration.GetValue<string>("Security:ValidIssuer"); // Using nested Json 
+```
+
+<h5>["myConfig"]</h5>
+```
+var defaultLogLevel = builder.Configuration["Logging:LogLevel:Default"];
+```
+
+## Options pattern (preferred)
+
+<h5>ConfigurationBinder.Bind</h5>
+
+```
+public class PositionOptions
+{
+    public const string Position = "Position";
+
+    public string Title { get; set; } = String.Empty;
+    public string Name { get; set; } = String.Empty;
+}
+```
+
+```
+var positionOptions = new PositionOptions();
+        Configuration.GetSection(PositionOptions.Position).Bind(positionOptions);
+
+        Console.WriteLine($"Title: {positionOptions.Title} \n" +
+                       $"Name: {positionOptions.Name}");
+```
+
+---
+ref : [microsoft doc](https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-7.0)
