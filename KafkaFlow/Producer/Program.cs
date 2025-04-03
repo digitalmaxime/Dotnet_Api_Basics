c1f5e95c6dff26c15cbe3e7489e23ab2462faf83
digitalmaxime <maxime.rheault@gmail.com>
2025-04-03 10:20:50 -0400

    schema

--- KafkaFlow/Producer/Program.cs
+++ KafkaFlow/Producer/Program.cs
@@ -1,13 +1,31 @@
-﻿using KafkaFlow;
-using KafkaFlow.Serializer;
+﻿using Confluent.SchemaRegistry;
+using KafkaFlow;
 using Microsoft.Extensions.DependencyInjection;
 using KafkaFlow.Producers;
 using Producer;
+// using KafkaFlow.SchemaRegistry;
+// using KafkaFlow.SchemaRegistry.Json;
+using System.Text.Json;
+using Confluent.SchemaRegistry.Serdes;
 
 var services = new ServiceCollection();
 
 const string topicName = "sample-topic";
 const string producerName = "say-hello";
+
+// var config = new SchemaRegistryConfig()
+// {
+//     Url = "http://localhost:8081",
+// };
+//
+// var schemaRegistryClient = new CachedSchemaRegistryClient(config);
+//
+// services.AddSingleton<ISchemaRegistryClient>(schemaRegistryClient);
+
+services.AddSingleton(new JsonSerializerOptions
+{
+    WriteIndented = true
+});
 
 services.AddKafka(
     kafka => kafka
@@ -15,14 +33,21 @@ services.AddKafka(
         .AddCluster(
             cluster => cluster
                 .WithBrokers(new[] { "localhost:9092" })
+                .WithSchemaRegistry(config => config.Url = "localhost:8081")
                 .CreateTopicIfNotExists(topicName, 1, 1)
                 .AddProducer(
                     producerName,
                     producer => producer
                         .DefaultTopic(topicName)
                         .AddMiddlewares(m =>
-                            m.AddSerializer<JsonCoreSerializer>()
-                        )
+                        {
+                            m.AddSchemaRegistryJsonSerializer<HelloMessage>(
+                                new JsonSerializerConfig
+                                {
+                                    SubjectNameStrategy = SubjectNameStrategy.TopicRecord,
+                                    
+                                });
+                        })
                 )
         )
 );
@@ -36,7 +61,7 @@ var producer = serviceProvider
 await producer.ProduceAsync(
     topicName,
     Guid.NewGuid().ToString(),
-    new HelloMessage { Text = "Hello2!" });
+    new HelloMessage { Text = "Hello from Json Schema!" });
 
 
 Console.WriteLine("Message sent!");
