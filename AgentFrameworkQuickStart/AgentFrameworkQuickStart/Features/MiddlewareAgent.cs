065dcec7571f43591f3fac859e3c06a1e7fb71a8
TREMMX <maxime.tremblay-rheault@bdc.ca>
2026-02-01 08:08:13 -0500

    MS Agent - fixed middleware du to updated version of package

--- AgentFrameworkQuickStart/AgentFrameworkQuickStart/Features/MiddlewareAgent.cs
+++ AgentFrameworkQuickStart/AgentFrameworkQuickStart/Features/MiddlewareAgent.cs
@@ -1,17 +1,7 @@
-using System.Collections;
-using System.Globalization;
-using AgentFrameworkQuickStart.Tools;
 using Azure;
 using Azure.AI.OpenAI;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
-using OpenAI;
-using AgentFrameworkQuickStart.Tools;
-using Azure;
-using Azure.AI.OpenAI;
-using Microsoft.Agents.AI;
-using Microsoft.Extensions.AI;
-using OpenAI;
 
 namespace AgentFrameworkQuickStart.Features;
 
@@ -20,27 +10,36 @@ public class MiddlewareAgent
     public static async Task<AIAgent> Call(string endpoint, string deploymentName, string apiKey)
     {
         Console.WriteLine("--- Middleware Agent ---");
-    
-        IChatClient client = new ChatClientBuilder(
-                new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey)).GetChatClient(deploymentName)
+
+        var client = new ChatClientBuilder( // this is an "IChatClient" not ChatClient
+                new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey))
+                    .GetChatClient(deploymentName)
                     .AsIChatClient())
             .Use(getResponseFunc: CustomChatClientMiddleware, getStreamingResponseFunc: null)
             .Build();
 
-        AIAgent agent = client.CreateAIAgent(
-                name: "MiddlewareAgent",
-                instructions:
-                "You are a utility assistant that can redirect to existing agents. When asked for this information, use your available tools.",
-                tools: []
-            );
-    
+        var client2 = new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey))
+            .GetChatClient(deploymentName)
+            .AsIChatClient()
+            .AsBuilder()
+            .Use(getResponseFunc: CustomChatClientMiddleware, getStreamingResponseFunc: null)
+            .Build();
+
+        AIAgent agent = new ChatClientAgent(
+            chatClient: client,
+            name: "MiddlewareAgent",
+            instructions:
+            "You are a utility assistant that can redirect to existing agents. When asked for this information, use your available tools.",
+            tools: []
+        );
+
         Console.WriteLine(await agent.RunAsync("Tell me a what day it is in a surprising way"));
         Console.WriteLine();
 
         return agent;
     }
-    
-    
+
+
     static async Task<ChatResponse> CustomChatClientMiddleware(
         IEnumerable<ChatMessage> messages,
         ChatOptions? options,
